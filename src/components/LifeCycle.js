0c1fb1b06139edd4b2c1c04273d3b681eac8ac1d
Chahed511 <76520296+Chahed511@users.noreply.github.com>
2021-03-29 02:42:24 +0200

    Update LifeCycle.js

--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -2,14 +2,40 @@ import React, { Component } from "react";
 class LifeCycle extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
+    this.state = {
+
+      date: new Date(),
+      time :0 
+		};
+
+    };
+  /*DidMount {this.state.time++}*/
   componentDidMount() {
     alert("component is mounted");
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
   }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+// componentDidUpdate
+componentDidUpdate(prevprops, prevstate) {
+  console.log("Component did update")
+  console.log(prevprops, prevstate)
+}
+tick() {
+  this.setState({
+    date: new Date()
+  });
+}
+
+
+
 
   render() {
-    return <h2></h2>;
+    return <h2>It is {this.state.date.toLocaleTimeString()} </h2>;
   }
 }
 
